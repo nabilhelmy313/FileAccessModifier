3ca1059e9058e7e259ec4697f1854a7feed2d27f
MTG <MTG@DESKTOP-FJOA5JS>
2022-11-14 13:42:06 +0200

    Add project files.

---
+++ ConsoleApp1/FileAccessModiferTestClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// the file access modifier is make it visible in its file only
+    /// </summary>
+    file  class FileAccessModiferTestClass
+    {
+        public void ConsoleMethodToSee()
+        {
+            Console.WriteLine("From Class Wtih  File Access modifier");
+        }
+    }
+
+    public class PrintConsoleClass
+    {
+        void ConsoleMessage()
+        {
+            FileAccessModiferTestClass fileAccess=new ();
+            fileAccess.ConsoleMethodToSee();
+        }
+    }
+}
